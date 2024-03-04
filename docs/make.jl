d025c18d1da2ae9cc53ca2dd5e309d6cc238e339
Janis Erdmanis <janiserdmanis@protonmail.ch>
2024-03-05 01:58:52 +0200

    updating documenter

--- docs/make.jl
+++ docs/make.jl
@@ -33,8 +33,10 @@ makedocs(
     sitename = "PeaceFounder.jl",
     repo = Documenter.Remotes.GitHub("PeaceFounder", "PeaceFounder.jl"),
     format = Documenter.HTML(
-        assets = [asset("https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.8/swagger-ui-bundle.js", class=:js, load_early=true),
-                  asset("https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.8/swagger-ui.css", class=:css)]
+        assets = [
+            asset("https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.8/swagger-ui-bundle.js", class=:js, load_early=true),
+            asset("https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.8/swagger-ui.css", class=:css, load_early=true) # for dark theme to work
+        ]
 
     ),
     modules = [PeaceFounder.Model, PeaceFounder.Client],
