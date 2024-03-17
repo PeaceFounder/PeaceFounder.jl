7f208b44e59210f7e5b5bbf5bec1e03b749b77db
Janis Erdmanis <janiserdmanis@protonmail.ch>
2024-03-17 15:55:11 +0200

    introducing braidchain ledger

--- test/.#store.jl
+++
@@ -1,1 +0,0 @@
-jerdmanis@Janiss-MacBook-Pro.local.21739
--- test/store.jl
+++ test/store.jl
@@ -1,2 +1,8 @@
 using PeaceFounder.Core.Store
 
+# generate content within the chain
+
+# store it within a temporary folder
+
+# read it from the folder
+
